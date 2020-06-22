c5273b9717c0643d195dd8bbb0d5be29e1a81a95
Umar Qureshi <dqureshiumar@gmail.com>
2020-06-23 01:33:17 +0530

    Updates

---
+++ precache-manifest.0537edb41744fa0c067eb4819b27d6d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526911b6e373ba9ad2818bdf85634a5f",
+    "url": "/React_Todo_App/index.html"
+  },
+  {
+    "revision": "cf14ded2352924bc2e58",
+    "url": "/React_Todo_App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b551659a076bf9d2da0d",
+    "url": "/React_Todo_App/static/js/2.a264906a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Todo_App/static/js/2.a264906a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf14ded2352924bc2e58",
+    "url": "/React_Todo_App/static/js/main.b7c2c034.chunk.js"
+  },
+  {
+    "revision": "c946f53f6566006c97db",
+    "url": "/React_Todo_App/static/js/runtime-main.601a8941.js"
+  }
+]);
